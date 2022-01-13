37e4a76d885775954e8003f09f8465a1ce316576
s19076 <s19076@pjwstk.edu.pl>
2022-01-13 20:35:50 +0100

    Added 15 examples

--- blueprints/examples/__init__.py
+++ blueprints/examples/__init__.py
@@ -6,9 +6,9 @@ blueprint = Blueprint("examples", __name__, url_prefix="/examples")
 
 @blueprint.route("/<int:example_id>", methods=["GET"])
 def get_example(example_id):
-    if example_id < 0 or example_id > 9:
+    if example_id < 0 or example_id > 15:
         return render_template("404.html")
-    if example_id == 9:
+    if example_id == 15:
         return render_template("example_last.html", example_id=example_id)
     if example_id == 0:
         return render_template("example_first.html", example_id=example_id)
