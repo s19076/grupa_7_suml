263f1289f944973d41d7b36aefc5ac3306bb258c
s19076 <s19076@pjwstk.edu.pl>
2022-01-13 23:45:39 +0100

    Added endpoint for uploading

--- blueprints/endpoints/__init__.py
+++ blueprints/endpoints/__init__.py
@@ -1,5 +1,9 @@
 # blueprints/endpoints/__init__.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
+from config import *
+from utils import allowed_file
 
 blueprint = Blueprint("index", __name__, url_prefix="/")
 
@@ -8,6 +12,27 @@ blueprint = Blueprint("index", __name__, url_prefix="/")
 def get_home():
     return render_template("home.html")
 
+
 @blueprint.route("examples")
 def get_examples():
     return render_template("examples.html")
+
+
+@blueprint.route("upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return render_template("upload_photo.html")
+
+        if file and allowed_file(file.filename, ALLOWED_EXTENSIONS):
+            filename = secure_filename(file.filename)
+
+            if not os.path.isdir(UPLOAD_FOLDER):
+                os.mkdir(UPLOAD_FOLDER)
+
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect("home")
+    return render_template("upload_photo.html")
