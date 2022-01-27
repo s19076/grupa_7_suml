ded2fa9ac4ea607004ae07ba5cdffb40784f89dc
s19076 <80158169+s19076@users.noreply.github.com>
2022-01-27 16:39:41 +0100

    Added function for downloading face restore model

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # main.py
 from flask import Flask
-# from blueprints.documented_endpoints import blueprint as documented_endpoint
+from utils import get_face_model
 from blueprints.endpoints import blueprint as endpoints
 from blueprints.examples import blueprint as example
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "TOKEN"
 
-# app.register_blueprint(documented_endpoint)
 app.register_blueprint(endpoints)
 app.register_blueprint(example)
+
+get_face_model()
 
 if __name__ == "__main__":
     app.run(debug = True)
