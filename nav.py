dca50ae66f02c04ad91bd203868a2f35d4977c86
s19076 <s19076@pjwstk.edu.pl>
2022-01-27 10:00:44 +0100

    Changed to objects from typing library

--- nav.py
+++ nav.py
@@ -1,5 +1,5 @@
 import typing as t
-
+from typing import List, Tuple
 from flask import url_for, Blueprint
 
 _nav_titles = {}
@@ -13,11 +13,11 @@ def register_title(title: str, *, blueprint: t.Optional[Blueprint] = None) -> t.
         _nav_titles[endpoint] = title
         return func
     return _reg
-
+    
 
 def get_title(endpoint: str) -> t.Optional[str]:
     return _nav_titles.get(endpoint)
 
 
-def make_path(endpoints: list[str]) -> list[tuple[str, str]]:
+def make_path(endpoints: List[str]) -> List[Tuple[str, str]]:
     return [(url_for(e), _nav_titles[e]) for e in endpoints]
