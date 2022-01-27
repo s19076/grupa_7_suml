62fa1f9b5c3afebc730fffb66fa31ac15f1f416e
kali <kali@kali.kali>
2022-01-27 16:18:22 +0100

    Added function for downloading face restore model

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+import subprocess
+import os
+
 def allowed_file(filename, allowed_extensions):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in allowed_extensions
+           
+def get_face_model():
+    if not os.path.exists(path = "model/face_model/GFPGANCleanv1-NoCE-C2.pth"):
+        subprocess.run(["wget",
+                        "https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth",
+                        "-P",
+                        "model/face_model"])
