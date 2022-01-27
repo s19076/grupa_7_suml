61cd1793dba979484fb3a56fec84ad43536c289a
s18539 <s18539@pjwstk.edu.pl>
2022-01-27 01:19:19 +0100

    Use a single template for all example pages

--- blueprints/examples/__init__.py
+++ blueprints/examples/__init__.py
@@ -2,15 +2,16 @@
 from flask import Blueprint, render_template, request
 
 blueprint = Blueprint("examples", __name__, url_prefix="/examples")
-
+MAX_EXAMPLE_ID = 15
 
 @blueprint.route("/<int:example_id>", methods=["GET"])
 def get_example(example_id):
-    if example_id < 0 or example_id > 15:
+    if example_id < 0 or example_id > MAX_EXAMPLE_ID:
         return render_template("404.html")
-    if example_id == 15:
-        return render_template("example_last.html", example_id=example_id)
-    if example_id == 0:
-        return render_template("example_first.html", example_id=example_id)
-    return render_template("example.html", example_id=example_id)
+
+    return render_template(
+        "example.html",
+        example_id = example_id,
+        max_example_id = MAX_EXAMPLE_ID,
+    )
 
