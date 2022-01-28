ece897c653141ab3a665727c8981b634c206a206
s19076 <s19076@pjwstk.edu.pl>
2022-01-28 10:43:50 +0100

    Added docstrings

--- utils.py
+++ utils.py
@@ -1,3 +1,24 @@
-def allowed_file(filename, allowed_extensions):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in allowed_extensions
+"""This module has some utility functions"""
+
+from typing import Set
+
+
+def allowed_file(file_name: str, allowed_extensions: Set[str]) -> bool:
+    """
+    Check if file has allowed extension from provided Set
+
+    Parameters
+    ----------
+    file_name, str
+        path to file which should be checked
+    allowed_extensions, Set[str]
+        Set of allowed extensions
+
+    Returns
+    -------
+    bool
+        True - if provided file has allowed extensions
+        False - otherwise
+    """
+    return '.' in file_name and \
+           file_name.rsplit('.', 1)[1].lower() in allowed_extensions
