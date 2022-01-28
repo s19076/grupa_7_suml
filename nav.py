552f39fefe7a2e5fde5a31df289415e778d8541e
s18539 <s18539@pjwstk.edu.pl>
2022-01-28 16:49:37 +0100

    Improve nav docs, remove redundant import

--- nav.py
+++ nav.py
@@ -1,11 +1,21 @@
-import typing as t
-from typing import List, Tuple
+"""
+This module contains support functions for rendering the navigation bar.
+The navigation bar lives in the ``_nav.html`` template. This template expects
+two parameters: ``page_title`` for displaying the current page's title and
+``nav`` for displaying the navigation path (also known as breadcrumbs).
+The ``nav`` parameter is a list of (endpoint_url, endpoint_title) pairs. It
+can be generated with the ``make_nav`` function, provided you've registered
+the titles for all endpoints you want to use with the ``register_title``
+decorator first.
+"""
+
+from typing import List, Tuple, Optional, Callable
 from flask import url_for, Blueprint
 
 _nav_titles = {}
 
 
-def register_title(title: str, *, blueprint: t.Optional[Blueprint] = None) -> t.Callable:
+def register_title(title: str, *, blueprint: Optional[Blueprint] = None) -> Callable:
     """Register a title for the specified route function.
 
     This title will be displayed in the navigation bar.
@@ -21,11 +31,16 @@ def register_title(title: str, *, blueprint: t.Optional[Blueprint] = None) -> t.
     return _reg
 
 
-def get_title(endpoint: str) -> t.Optional[str]:
+def get_title(endpoint: str) -> Optional[str]:
     """Get a title assigned to the specified endpoint.
 
-    ``endpoint`` is a ``flask.url_for``-compatible endpoint
-    identifier. Refer to the Flask documentation for more details.
+    If no title is assigned, the return value is None.
+
+    Parameters
+    ----------
+    endpoint: str
+        A ``flask.url_for``-compatible endpoint identifier.
+        Refer to the Flask documentation for more details.
     """
     return _nav_titles.get(endpoint)
 
@@ -37,5 +52,12 @@ def make_path(endpoints: List[str]) -> List[Tuple[str, str]]:
     function must be passed to the template renderer in the ``nav``
     argument. The template must include the ``_nav.html`` subtemplate
     as well.
+
+    Parameters
+    ----------
+    endpoints: List[str]
+        A list of ``flask.url_for``-compatible endpoint identifiers.
+        Each endpoint should have its title registered using the
+        ``register_title`` decorator.
     """
     return [(url_for(e), _nav_titles[e]) for e in endpoints]
